0e8cb325ac47942c952104bb243837ba7cd02fc7
HariKarthyk <hari.jsmith494@gmail.com>
2021-06-28 19:16:00 +0530

    Corrections

---
+++ controllers/auth.js
@@ -0,0 +1,67 @@
+// @type middleware
+
+const {StatusCodes} = require("http-status-codes");
+
+// @desc To set userInfo to the req.user
+exports.setUser = (req, res, next, id) => {
+	if (!id) {
+		return res.status(StatusCodes.BAD_REQUEST).json({
+			error: true,
+			message: "user Id Not Available",
+		});
+	}
+	User.findOne({_id: id}, (error, userInfo) => {
+		if (error) {
+			return res.status(StatusCodes.BAD_REQUEST).json({
+				error: true,
+				message: "Error in finding the user Id",
+			});
+		}
+		userInfo.password = undefined;
+		req.user = userInfo;
+		next();
+	});
+};
+
+// @type middleware
+// @desc To check whether the user Signed in properly
+exports.isSignedIn = (req, res, next) => {
+	const bearerHeader = req.headers["authorization"];
+
+	if (bearerHeader) {
+		const bearer = bearerHeader.split(" ");
+		const bearerToken = bearer[1];
+		if (!req.user || bearerToken !== req.user.jwtToken) {
+			return res.status(400).json({
+				error: true,
+				message: "Un authorized access",
+			});
+		}
+		next();
+	} else {
+		return res.status(StatusCodes.BAD_REQUEST).json({
+			error: true,
+			message: "No token found",
+		});
+	}
+};
+
+exports.isOwner = (req, res, next) => {
+	if (!req.body || !req.user || req.user.role !== "owner") {
+		return res.status(StatusCodes.BAD_REQUEST).json({
+			error: true,
+			message: "UnAuthorized Access",
+		});
+	}
+	next();
+};
+
+exports.isAdmin = (req, res, next) => {
+	if (!req.body || !req.user || req.user.role !== "admin") {
+		return res.status(StatusCodes.BAD_REQUEST).json({
+			error: true,
+			message: "UnAuthorized Access",
+		});
+	}
+	next();
+};
