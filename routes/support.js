bd44a17a2e72b4a0d79ed618af1a52be4555ee0f
Hari Karthikkeyyan <harikarthikkeyyan@Haris-MacBook-Pro.local>
2021-07-27 22:59:42 +0530

    support controller and route modified .

--- routes/support.js
+++ routes/support.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { isSignedIn } = require("../controllers/auth");
+const { createSupport, updateSupport ,getSupportList, supportDescription } = require("../controllers/support");
 const router = express.Router();
 
-router.post("/support/new/:userId",isSignedIn , createNewSupport);
+router.post("/support/new/:userId",isSignedIn ,createSupport );
 
 router.put('/support/add/:userId/:supportId',isSignedIn,updateSupport);
+
+router.get('/support/list/:userId' , isSignedIn , getSupportList);
+
+router.get('/support/:userId/:supportId',isSignedIn , supportDescription); 
 
 module.exports = router;
