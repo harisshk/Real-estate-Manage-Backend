d021e296b4a8473eb855be4d06a137162c050825
Hk <hk@Hks-MacBook-Pro.local>
2021-08-01 18:58:07 +0530

    support id

--- controllers/support.js
+++ controllers/support.js
@@ -1,111 +1,127 @@
 const { StatusCodes } = require("http-status-codes");
 const Support = require("../models/support")
 
-exports.createSupport = async(req,res) => {
-    try{
-        let support = await new Support(req.body).save();
-        console.log(support);
-        return res.status(StatusCodes.OK).json({
-            error : false ,
-            message : "success",
-            support :support,
+exports.createSupport = async (req, res) => {
+    Support.find({})
+        .countDocuments()
+        .then((count) => {
+            let newSupportCount = {
+                ...req.body,
+                supportNo: String(count + 1000 + 1)
+            }
+            let newSupport = new Support(newSupportCount)
+            newSupport.save()
+                .then((support) => {
+                    return res.status(StatusCodes.OK).json({
+                        error: false,
+                        message: "success",
+                        support: support
+                    })
+                        .catch((error) => {
+                            return res.status(StatusCodes.BAD_REQUEST).json({
+                                error: true,
+                                err: error.message,
+                                message: "Error in creating the support",
+                            })
+                        })
+                })
+                .catch((error) => {
+                    return res.status(StatusCodes.BAD_REQUEST).json({
+                        error: true,
+                        err: error.message,
+                        message: "Error in creating the support",
+                    })
+                })
         })
-    }catch(error){
-        console.log(error);
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            error : true,
-            err : error.message,
-            message : "Error in creating the support",
-        })
-    }
-
 }
 
-exports.updateSupport = async (req,res) => {
-    try{
-        let support = await Support.findOneAndUpdate({_id : req.params.supportId},{$set : req.body},{new : true});
-        return res.status(StatusCodes.OK).json({
-            error : false ,
-            message : "success",
-            support :support,
-        })
-    }catch(error){
-        console.log(error);
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            error : true,
-            err : error.message,
-            message : "Error in updating the support",
-        })
-    }
-}
 
-exports.getSupportList = async(req,res) => {
-    try{
-        let supports = await Support.find({user : req.params.userId});
+
+exports.updateSupport = async (req, res) => {
+    try {
+        let support = await Support.findOneAndUpdate({ _id: req.params.supportId }, { $set: req.body }, { new: true });
         return res.status(StatusCodes.OK).json({
-            error : false ,
-            message : "success",
-            supports :supports,
+            error: false,
+            message: "success",
+            support: support,
         })
-    }catch(error){
+    } catch (error) {
         console.log(error);
         return res.status(StatusCodes.BAD_REQUEST).json({
-            error : true,
-            err : error.message,
-            message : "Error in updating the support",
+            error: true,
+            err: error.message,
+            message: "Error in updating the support",
         })
     }
 }
 
-exports.supportDescription = async(req,res) => {
-    try{
-        let support = await Support.findOne({_id : req.params.supportId});
+exports.getSupportList = async (req, res) => {
+    try {
+        let supports = await Support.find({ user: req.params.userId });
         return res.status(StatusCodes.OK).json({
-            error : false ,
-            message : "success",
-            support :support,
+            error: false,
+            message: "success",
+            supports: supports,
         })
-    }catch(error){
+    } catch (error) {
         console.log(error);
         return res.status(StatusCodes.BAD_REQUEST).json({
-            error : false,
-            err : error.message,
-            message : "Error in updating the support",
+            error: true,
+            err: error.message,
+            message: "Error in updating the support",
         })
     }
 }
-exports.getAllSupportByAdmin = async(req,res) => {
-    try{
+
+exports.supportDescription = async (req, res) => {
+    try {
+        let support = await Support.findOne({ _id: req.params.supportId });
+        return res.status(StatusCodes.OK).json({
+            error: false,
+            message: "success",
+            support: support,
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            error: false,
+            err: error.message,
+            message: "Error in updating the support",
+        })
+    }
+}
+exports.getAllSupportByAdmin = async (req, res) => {
+    try {
         let supports = await Support.find({});
         return res.status(StatusCodes.OK).json({
-            error : false ,
-            message : "success",
-            supports :supports,
+            error: false,
+            message: "success",
+            supports: supports,
         })
-    }catch(error){
+    } catch (error) {
         console.log(error);
         return res.status(StatusCodes.BAD_REQUEST).json({
-            error : true,
-            err : error.message,
-            message : "Error in updating the support",
+            error: true,
+            err: error.message,
+            message: "Error in updating the support",
         })
     }
 }
 
-exports.getAllSupportByRegionalAdmin = async(req,res) => {
-    try{
-        let supports = await Support.find({region : req.body.region});
+exports.getAllSupportByRegionalAdmin = async (req, res) => {
+    try {
+        let supports = await Support.find({ region: req.body.region });
         return res.status(StatusCodes.OK).json({
-            error : false ,
-            message : "success",
-            supports :supports,
+            error: false,
+            message: "success",
+            supports: supports,
         })
-    }catch(error){
+    } catch (error) {
         console.log(error);
         return res.status(StatusCodes.BAD_REQUEST).json({
-            error : true,
-            err : error.message,
-            message : "Error in updating the support",
+            error: true,
+            err: error.message,
+            message: "Error in updating the support",
         })
     }
 }
