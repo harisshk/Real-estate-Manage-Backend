99974fd57ec0d37703f91f259d5fdde4f945c847
Hk <hk@Hks-MacBook-Pro.local>
2021-09-09 19:45:53 +0530

    activity filter

--- controllers/activity.js
+++ controllers/activity.js
@@ -17,3 +17,21 @@ exports.getUserActivities = async(req,res) => {
         })
     }
 }
+
+exports.filterByDate = async(req,res) => {
+    const { startDate, endDate} = req.body
+    try{
+        let activities = await Activities.find({createdAt:{$gt:startDate,$lt:endDate}}).populate('user').populate('updatedBy');
+        return res.status(StatusCodes.OK).json({
+            error : false,
+            message :"success",
+            activities : activities,
+        })
+    }catch(error){
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            error : true,
+            err: error.message,
+            message :"Error in fetching the User Activities",
+        })
+    }
+}
--- routes/activity.js
+++ routes/activity.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { setUser, isAdmin, isSignedIn, isOwner, isRegionalAdmin} = require("../controllers/auth");
-const {getUserActivities} = require('../controllers/activity');
+const {getUserActivities, filterByDate} = require('../controllers/activity');
 const router = express.Router();
 
 router.param('userId', setUser);
 
 router.get('/activity/user/logs',getUserActivities);
+router.post('/activity/filter',filterByDate);
 
 
 module.exports = router;
